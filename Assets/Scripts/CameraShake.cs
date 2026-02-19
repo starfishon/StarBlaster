e52ee8cb38f2f1ea4df60c868e37c660123ea1d6
peri.gold81@gmail.com <peri.gold81@gmail.com>
2026-02-19 21:00:28 +0200

    camera

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+
+    [SerializeField] float _shaeDuration=0.5f;
+    [SerializeField] float _shakeMagnitude = 0.5f;
+    Vector3 _initialPosition;
+
+
+    void Start()
+    {
+        _initialPosition = transform.position;
+    }
+
+    public void plary()
+    {
+        StartCoroutine(ShakeCamera());
+    }
+    IEnumerator  ShakeCamera()
+    {
+        transform.position = _initialPosition +
+        Random.insideUnitCircle * _shakeMagnitude;
+        
+        
+      return null;  
+    }
+}
